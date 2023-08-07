e94d4cca12eaed8212c8d716e50de1e4e233ee0d
Octavian Sima <octavian.sima98@gmail.com>
2023-08-07 19:15:57 +0000

    anonymize -> sanitize

--- python-package-prompt-guard/src/promptguard/promptguard_service.py
+++ python-package-prompt-guard/src/promptguard/promptguard_service.py
@@ -15,58 +15,61 @@ import requests
 # domain name manually.
 PROMPTGUARD_SERVICE_DOMAIN_NAME = os.environ.get("PROMPTGUARD_SERVICE_DOMAIN_NAME")
 
+
 @dataclass
-class AnonymizeResponse:
-    anonymized_text: str
+class SanitizeResponse:
+    sanitized_text: str
     secret_entropy: bytes
 
-def anonymize(text: str) -> AnonymizeResponse:
+
+def sanitize(text: str) -> SanitizeResponse:
     """
-    Takes in a text prompt and returns the anonymized
+    Takes in a text prompt and returns the sanitized
     text with PII redacted from it.
 
     Parameters
     ----------
     text : str
-        Prompt that you want to be anonymized.
+        Prompt that you want to be sanitized.
 
     Returns
     -------
-    AnonymizeResponse
+    SanitizeResponse
         The anonymzied version of text without PII and
         a secret entropy value.
     """
-    response = _send_request_to_ppp_service(
-        endpoint="anonymize", payload={"text": text}
-    )
-    return AnonymizeResponse(**json.loads(response.text))
+    response = _send_request_to_ppp_service(endpoint="sanitize", payload={"text": text})
+    return SanitizeResponse(**json.loads(response.text))
+
 
 @dataclass
-class DeanonymizeResponse:
-    deanonymized_text: str
+class DesanitizeResponse:
+    desanitized_text: str
 
-def deanonymize(anonymized_text: str, secret_entropy: bytes) -> DeanonymizeResponse:
+
+def desanitize(sanitized_text: str, secret_entropy: bytes) -> DesanitizeResponse:
     """
-    Takes in an anonymized response and returns the deanonymized
+    Takes in a sanitized response and returns the desanitized
     text with PII added back to it.
 
     Parameters
     ----------
-    anonymized_text : str
-        Anonymized response that you want to be deanonymized.
+    sanitized_text : str
+        Sanitized response that you want to be desanitized.
     secret_entropy : bytes
         Secret entropy value that should have been returned by
-        the call to `anonymize`.
+        the call to `sanitize`.
 
     Returns
     -------
-    DeanonymizeResponse
-        The deanonymzied version of `anonymized_text` with PII added back in.
+    DesanitizeResponse
+        The deanonymzied version of `sanitized_text` with PII added back in.
     """
     response = _send_request_to_ppp_service(
-        endpoint="deanonymize", payload={"anonymized_text": anonymized_text, "secret_entropy": secret_entropy}
+        endpoint="desanitize",
+        payload={"sanitized_text": sanitized_text, "secret_entropy": secret_entropy},
     )
-    return DeanonymizeResponse(**json.loads(response.text))
+    return DesanitizeResponse(**json.loads(response.text))
 
 
 ########## Helper Functions ##########
@@ -96,7 +99,12 @@ def _send_request_to_ppp_service(
     """
     endpoint_url = f"http://{PROMPTGUARD_SERVICE_DOMAIN_NAME}/{endpoint}"
     access_token = get_access_token()
-    response = requests.request("POST", endpoint_url, headers={"Authorization": f"Bearer {access_token}"}, data=json.dumps(payload))
+    response = requests.request(
+        "POST",
+        endpoint_url,
+        headers={"Authorization": f"Bearer {access_token}"},
+        data=json.dumps(payload),
+    )
     if response.status_code != HTTPStatus.OK:
         raise Exception(
             f"Error response from {endpoint_url}: "
